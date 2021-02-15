6a45147d2b4e6a49258af50f73a007769b84239c
LaibaFatimaKhan <LaibaFatimaKhan@users.noreply.github.com>
2021-02-15 15:58:48 +0500

    added

---
+++ Computer Science/Algorithms/Naive.py
@@ -0,0 +1,58 @@
+G =  [[ 0, 3, 3, 0, 0, 0 ],  # s
+     [ 0, 0, 2, 3, 0, 0 ],  # o
+     [ 0, 0, 0, 0, 2, 0 ],  # p
+     [ 0, 0, 0, 0, 4, 2 ],  # q
+     [ 0, 0, 0, 0, 0, 2 ],  # r
+     [ 0, 0, 0, 0, 0, 0 ]]  # t
+F =  [[ 0, 0, 0, 0, 0, 0 ],  # s
+     [ 0, 0, 0, 0, 0, 0 ],  # o
+     [ 0, 0, 0, 0, 0, 0 ],  # p
+     [ 0, 0, 0, 0, 0, 0 ],  # q
+     [ 0, 0, 0, 0, 0, 0 ],  # r
+     [ 0, 0, 0, 0, 0, 0 ]]  # t
+
+def dfs(G,F,s,t):
+    stack = [s]
+    path = {s:[]}
+    
+
+    while(stack):
+        print("stack:",stack)
+        u = stack.pop()
+        for v in range(len(G)):
+            print("u:,",u,"v:",v)
+            print("path:",path)
+            if (F[u][v] < G[u][v]) and v not in path:
+                path[v] = path[u]+[(u,v)]
+                if v==t:
+                    print("dfs: path: ",path)
+                    
+                    return path[v]
+                stack.append(v)
+                
+    return None
+
+def main(G,F,s,t):
+    print("G:",G)
+    path = dfs(G,F,s,t)
+    print("Initial:",path)
+    max_flow = 0
+    while (path!=None):
+        flow = min(G[u][v] - F[u][v] for u,v in path)
+        for u,v in path:
+            print("u,v",u,v)            
+            F[u][v]+=flow
+        path = dfs(G,F,s,t)
+        print("path:",path)
+    print("Flow:",max_flow)
+    return sum(F[s][i] for i in range(len(G)))
+
+# using time() to show present time 
+start=time.time()
+print(main(G,F,0,5))
+# using time() to show present time 
+end=time.time()
+print("Sigma/Diff:  ", end-start)
+
+    
+    
